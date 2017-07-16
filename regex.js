1432a0385256fe5e1e7e9d6d6b2cd0877ffaee15
alexander-io <abharris@pugetsound.edu>
2017-07-16 10:07:44 -0700

    ++matching specified characters, excluding specified characters

--- regex.js
+++ regex.js
@@ -11,6 +11,28 @@
    any_digit : '\\d',
    any_non_digit_char : '\\D',
    any_char : '.',
-   
+   period : '\\.',
+
  }
-console.log(regex.any_digit);
+
+for (x in regex) {
+  console.log(regex[x]);
+}
+
+/*
+ * regex, matching specified characters
+ * https://regexone.com/lesson/matching_characters?
+ *
+ * there is a method for matching specific characters using regular expressions, by defining them inside square brackets. for example, the pattern [abc] will only match a single a, b, or c letter and nothing else
+ * regex : [cmf]an, will return 'can', 'man
+, and 'fan'
+ */
+
+/*
+ * regex, excluding specific characters
+ *
+ * use a similar expression to exclude specific characters, square brackets [] and the ^ hat
+ *
+ * regex : [^b]og will return 'hog' and 'dog', but avoid 'bog'
+ *
+ */
