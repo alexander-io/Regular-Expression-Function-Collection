bd1fbfe14d0b6e36682161cf013095a3007dba42
alexander-io <abharris@pugetsound.edu>
2017-07-16 09:50:00 -0700

    ++init regex collection

---
+++ regex.js
@@ -0,0 +1,16 @@
+// regular expressions
+
+/*
+ *
+ *
+ *
+ *
+ */
+
+ let regex = {
+   any_digit : '\\d',
+   any_non_digit_char : '\\D',
+   any_char : '.',
+   
+ }
+console.log(regex.any_digit);
